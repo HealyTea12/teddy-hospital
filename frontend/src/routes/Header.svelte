64eb3aa689a8f8915e726c49f563e3f8b0f889ea
Kento Verlaan <kento.verlaan@stud.uni-heidelberg.de>
2025-05-27 02:32:25 +0200

    add results to header and simple generate button under admin

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -28,6 +28,9 @@
 			<li aria-current={page.url.pathname === '/camera' ? 'page' : undefined}>
 				<a href="/camera">Camera</a>
 			</li>
+			<li aria-current={page.url.pathname === '/results' ? 'page' : undefined}>
+				<a href="/results">Results</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
