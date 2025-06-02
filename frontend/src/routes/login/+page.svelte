2588f38cd6df019f1c92c3400bba87ca141b2dbe
Cavalcanti, Tarik <tarikcavalcanti12@gmail.com>
2025-06-02 11:41:03 +0200

    frontend part 2

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,22 +1,38 @@
 <script lang="ts">
-        
-    import { enhance } from '$app/forms';
+	import { goto } from "$app/navigation";
+    import { PUBLIC_BACKEND_URL } from '$env/static/public';
+
+    let password: string = $state('');
+    let errorMessage: string = $state('');
     const form = $props();
-    
+    async function login() {
+        let data = new FormData();
+        data.append('password', password);
+        const response = await fetch(`${PUBLIC_BACKEND_URL}/token`, {
+            method: 'POST',
+            body: data,
+        });
+        console.log(response);
+        if(response.ok){
+            const json = await response.json();
+            localStorage.setItem('session', json.access_token);
+            await goto('/');
+        } else {
+            errorMessage = `Error: ${response.statusText}`;
+        }
+    }    
 </script>
 
 <h1>
     Login
 </h1>
-<form method="POST" use:enhance>
     <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <button type="submit">Login</button>
+    <input type="password" id="password" name="password" bind:value={password} required>
+    <button onclick={login}>Login</button>
     
-{#if form?.error}
-    <p class="error">{form.error}</p>
-{/if}
-</form>
+
+    <p class="error">{errorMessage}</p>
+
 
 
 
